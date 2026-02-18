f53debc9c3f4123cff3b55e273f793cd0843b2aa
fedorenkoivan <ivan.r.fedorenko@gmail.com>
2026-02-18 21:54:09 +0200

    test commit to see whether my evil twin is still author of my work

--- lab2/team.ts
+++ lab2/team.ts
@@ -2,7 +2,8 @@ interface Student {
     slug: string;
     fullName: string;
     lovesMom: boolean;
-    favouriteDb?: string;
+    bio: string;
+    favouriteDb: string;
 }
 
 export const team: Student[] = [
@@ -10,11 +11,14 @@ export const team: Student[] = [
         slug: "fedorenko",
         fullName: "Ivan Fedorenko",
         lovesMom: true,
+        bio: "A passionate software developer.",
         favouriteDb: "PostgreSQL"
     },
     {
         slug: "fring",
         fullName: "Gustavo Fring",
-        lovesMom: false
+        lovesMom: false,
+        bio: "A ruthless businessman.",
+        favouriteDb: "MongoDB"
     }
 ];
