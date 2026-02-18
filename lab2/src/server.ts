04881dcdd776dd286b699c6aa5066bbd441bf1f5
Ivan <i.fedorenko@headway.inc>
2026-02-18 21:15:07 +0200

    feat: add minimal shablon

--- lab2/src/server.ts
+++ lab2/src/server.ts
@@ -3,9 +3,20 @@ import express, { type Request, type Response } from 'express';
 const app = express();
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req: Request, res: Response) => {
     res.send('<h1>hello</h1>');
 });
+
+const person = {
+    name: "Fedorenko",
+    lovesMom: true
+}
+
+app.get('/fedorenko', (req: Request, res: Response) => {
+    res.render('index', { person })
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
