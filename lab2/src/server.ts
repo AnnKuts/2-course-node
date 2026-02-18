97c0e1e0fdce5cc4dfb8acc2c8b4e951c97f7c3d
Ivan <i.fedorenko@headway.inc>
2026-02-18 21:36:03 +0200

    slug as a param, err page for team/weirdparam

--- lab2/src/server.ts
+++ lab2/src/server.ts
@@ -10,14 +10,16 @@ app.get('/', (req: Request, res: Response) => {
     res.send('<h1>hello</h1>');
 });
 
-app.get('/fedorenko', (req: Request, res: Response) => {
-    const student = team.find(s => s.fullName === "Ivan Fedorenko");
-    res.render('index', { student })
-})
-app.get('/fring', (req: Request, res: Response) => {
-    const student = team.find(s => s.slug === "fring");
-    res.render('index', { student })
-})
+app.get('/team/:slug', (req: Request, res: Response) => {
+    const requestSlug = req.params.slug;
+    const student = team.find(student => student.slug === requestSlug);
+
+    if (student) {
+        res.render('index', { student });
+    } else {
+        res.status(404).send('Student not found');
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
