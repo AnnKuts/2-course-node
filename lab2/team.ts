01163609c8e747c342abd7b0b2c2a81af3b9f947
Impe11e <1mpellee@gmail.com>
2026-02-19 20:37:34 +0200

    feat: add new params for ejs and styles for the main page

--- lab2/team.ts
+++ lab2/team.ts
@@ -3,8 +3,12 @@ interface Student {
     fullName: string;
     imagePath: string;
     bio: string;
-    socialLink: string;
     favouriteDb: string;
+    technologies: string[];
+    hobbies: string[];
+    favoriteQuote: string;
+    favoriteMovie: string;
+    socialLink: string;
 }
 
 export const team: Student[] = [
@@ -13,15 +17,35 @@ export const team: Student[] = [
         fullName: "Ivan Fedorenko",
         imagePath: "/assets/gustavo.jpg",
         bio: "A passionate software developer.",
-        socialLink: "https://www.linkedin.com/in/fedorenkoivan/",
-        favouriteDb: "PostgreSQL"
+        favouriteDb: "PostgreSQL",
+        technologies: [],
+        hobbies: [],
+        favoriteQuote: "",
+        favoriteMovie: "",
+        socialLink: "https://www.linkedin.com/in/fedorenkoivan/"
     },
     {
         slug: "loban",
         fullName: "Mykhailo Loban",
         imagePath: "/assets/gustavo.jpg",
         bio: "A ruthless businessman.",
-        socialLink: "https://www.linkedin.com/in/mykhailo-loban-a6a10a383/",
-        favouriteDb: "MongoDB"
+        favouriteDb: "MongoDB",
+        technologies: [],
+        hobbies: [],
+        favoriteQuote: "",
+        favoriteMovie: "",
+        socialLink: "https://www.linkedin.com/in/mykhailo-loban-a6a10a383/"
+    },
+        {
+        slug: "khorunzha",
+        fullName: "Mariia Khorunzha",
+        imagePath: "/assets/clem.png",
+        bio: "An outstanding FIOT student.",
+        favouriteDb: "MongoDB",
+        technologies: ["React", "Node.js", "Fastify", "PostgreSQL", "Jest", "Docker", "Postman", "Figma"],
+        hobbies: ["Reading", "Videogames", "Sports"],
+        favoriteQuote: "not every wrapper is a decorator, but every decorator is a wrapper",
+        favoriteMovie: "Before i fall",
+        socialLink: "https://www.linkedin.com/in/mariia-khorunzha-93072b336/"
     }
 ];
