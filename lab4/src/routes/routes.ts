865c573b7fa9c2bbf05670412b57bab5c032a728
LobanMihajlo <loban.mihajlo2411@gmail.com>
2026-04-28 14:27:44 +0300

    feat: implement auth functionality using postgresql

--- lab4/src/routes/routes.ts
+++ lab4/src/routes/routes.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 const router = express.Router();
 import { getAllFanficsController } from '../controllers/controller.ts';
+import { registerController, loginController, meController } from '../controllers/authController.ts';
+import { authenticateToken } from '../middleware/authMiddleware.ts';
 
 router.get('/', getAllFanficsController);
+
+router.post('/auth/register', registerController);
+router.post('/auth/login', loginController);
+router.get('/auth/me', authenticateToken, meController);
 
 export default router;
