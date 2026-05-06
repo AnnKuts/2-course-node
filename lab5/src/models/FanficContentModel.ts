4303c3eb1579280e7e7f8aaf03326ffe26a2e206
fedorenkoivan <ivan.r.fedorenko@gmail.com>
2026-05-06 22:28:49 +0300

    Feat: lab5
    Co-authored-by: Fedorenko Ivan <ivan.r.fedorenko@gmail.com>
    Co-authored-by: Kuts Anna <kuutsann@gmail.com>
    Co-authored-by: Loban Mihajlo <loban.mihajlo2411@gmail.com>
    Co-authored-by: Mariia Khorunzha <1mpellee@gmail.com>

---
+++ lab5/src/models/FanficContentModel.ts
@@ -0,0 +1,22 @@
+import { Model, DataTypes, InferAttributes, InferCreationAttributes } from 'sequelize';
+import sequelize from '../sequelize.ts';
+
+export class FanficContentModel extends Model<
+    InferAttributes<FanficContentModel>,
+    InferCreationAttributes<FanficContentModel>
+> {
+    declare fanfic_id: string;
+    declare content: string | null;
+}
+
+FanficContentModel.init({
+    fanfic_id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+    },
+    content: { type: DataTypes.TEXT, allowNull: true },
+}, {
+    sequelize,
+    tableName: 'fanfic_contents',
+    timestamps: false,
+});
---
+++ lab5/src/services/commentService.ts
@@ -0,0 +1,31 @@
+import { UUID } from 'node:crypto';
+import { Comment } from '../models/comment.ts';
+import {
+    getCommentsByFanficIdAsync,
+    getCommentByIdAsync,
+    insertCommentAsync,
+    updateCommentAsync,
+    deleteCommentAsync,
+} from '../repository/commentRepo.ts';
+
+export const getByFanficIdAsync = async (fanfic_id: string): Promise<Comment[]> => {
+    return await getCommentsByFanficIdAsync(fanfic_id);
+};
+
+export const getByIdAsync = async (comment_id: UUID): Promise<Comment | null> => {
+    return await getCommentByIdAsync(comment_id);
+};
+
+export const createAsync = async (
+    comment: Omit<Comment, 'comment_id' | 'created_at'>
+): Promise<Comment> => {
+    return await insertCommentAsync(comment);
+};
+
+export const updateAsync = async (comment: Comment): Promise<void> => {
+    await updateCommentAsync(comment);
+};
+
+export const deleteAsync = async (comment_id: UUID): Promise<boolean> => {
+    return await deleteCommentAsync(comment_id);
+};
